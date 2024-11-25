de1b0b5fbdc8820bacb2544f976a7456d553ec8b
Abdel1117 <abderahmane.adjali@live.fr>
2024-11-25 16:14:57 +0100

    first commit

---
+++ src/Entity/Produit/Produit.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+class Produit {
+
+    private int $id;
+    private string $nom;
+    private string $description;
+
+    private float $prix;
+
+    private int $stock;
+
+    public function __construct($id, $nom, $description, $prix, $stock){
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->description = $description;
+        $this->prix = $prix;
+        $this->stock = $stock;
+    }
+    
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     */ 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */ 
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prix
+     */ 
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /**
+     * Set the value of prix
+     *
+     * @return  self
+     */ 
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of stock
+     */ 
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    /**
+     * Set the value of stock
+     *
+     * @return  self
+     */ 
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+}
+
+
+?>
